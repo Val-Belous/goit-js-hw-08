c735ebf1759135456fc016a67eb1398110b98da7
Val-Belous <vaaalbelous@gmail.com>
2022-06-21 22:51:33 +0300

    done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const result = galleryItems
+  .map(element => {
+    return `
+  <a class="gallery__item" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', result);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+let inputs = {};
+initForm();
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  if (email === '' || message === '') {
+    alert('Всі поля повинні бути заповнені');
+    return;
+  }
+  const formData = new FormData(form);
+  formData.forEach(() => {
+    form.reset();
+    localStorage.clear();
+  });
+
+  console.log(inputs);
+}
+
+form.addEventListener('input', throttle(handlerChange, 500));
+
+function handlerChange(evt) {
+  inputs[evt.target.name] = evt.target.value;
+  //   console.log(inputs);
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputs));
+}
+
+function initForm() {
+  let saveForm = localStorage.getItem('feedback-form-state');
+  if (saveForm) {
+    saveForm = JSON.parse(saveForm);
+    // console.log(saveForm);
+    Object.entries(saveForm).forEach(([name, value]) => {
+      //   console.log(name, value);
+      //   console.log(form.elements);
+      inputs[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
